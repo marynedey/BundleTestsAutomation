2d02e64180ca99a31753d28a926783ad5f61f3f5
DEY Maryne <m.dey@Viveris.lan>
2025-08-27 14:45:45 +0200

    Calcul du SHA256

---
+++ utils/HashUtils.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BundleTestsAutomation
+{
+    public static class HashUtils
+    {
+        // SHA-256 d'une chaîne
+        public static string ComputeSha256Hash(string text)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(text);
+                byte[] hashBytes = sha256.ComputeHash(bytes);
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToUpperInvariant();
+            }
+        }
+
+        // SHA-256 d'un fichier
+        public static string ComputeSha256HashFromFile(string filePath)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            using (var stream = File.OpenRead(filePath))
+            {
+                byte[] hashBytes = sha256.ComputeHash(stream);
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToUpperInvariant();
+            }
+        }
+    }
+}
