bbb021a5e09ca92e43bcb516cff0c2932df89385
DEY Maryne <m.dey@Viveris.lan>
2025-09-03 08:55:32 +0200

    Fichier pour la standardisation des résultats de tests

---
+++ Services/LogTesters/TestResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BundleTestsAutomation.Services.LogTesters
+{
+    public class TestResult
+    {
+        public string TestName { get; set; } = "";
+        public List<string> Errors { get; set; } = new List<string>();
+        public bool HasErrors => Errors.Count > 0;
+        public override string ToString()
+        {
+            if (!HasErrors) return $"{TestName}: OK";
+            return $"{TestName}: KO\r\n- {string.Join("\r\n- ", Errors)}";
+        }
+    }
+}
